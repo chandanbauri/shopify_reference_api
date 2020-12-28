79cb264003fd8cf02e703a97d5803dddc01ff8b5
chandanbauri <chandanbauri441@gmail.com>
2020-12-28 19:06:50 +0530

    c-2 28/12/2020

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,10 @@ const db = new DB();
 router.post("/db/insertone", db.InsertOne);
 // api request to get the list of docs of the specified collection
 router.get("/db/itemlist", db.getList);
-// api request to get a document from the specified id and collection
-router.get("/db/itembyid", db.getItemById)
+// api request to get a document using the specified id and collection
+router.get("/db/itembyid", db.getItemById);
+// api request to delete a document using the specified id and collection
+router.delete("/db/delete",db.findAndDelete);
 
 
 module.exports = router;
